de305bc080c27e3598d41eae0933cf79c056e5db
luiz2k <luizteles934@gmail.com>
2024-10-11 12:45:34 -0300

    Configurar o Json Web Token no AppModule

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -5,9 +5,24 @@ import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TaskModule, UserModule, AuthModule, DatabaseModule],
+  imports: [
+    TaskModule,
+    UserModule,
+    AuthModule,
+    DatabaseModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: process.env.JWT_EXPIRES_IN,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
