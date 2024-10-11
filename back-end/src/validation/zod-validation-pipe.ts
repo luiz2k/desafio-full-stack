56849fe4ed9d4cf3bbbb6a990b0b8e3550cdd04c
luiz2k <luizteles934@gmail.com>
2024-10-11 12:02:47 -0300

    Implementar classe responsável pela validação com o Zod

---
+++ back-end/src/validation/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { ZodSchema } from 'zod';
+
+// Reponsável por validar os dados com o Zod
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      throw new BadRequestException({
+        message: 'Erro na validação dos dados',
+        data: error.issues,
+      });
+    }
+  }
+}
