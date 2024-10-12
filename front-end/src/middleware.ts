a71bccae9c9bb00ab7015066b1d215442bdb9bdc
luiz2k <luizteles934@gmail.com>
2024-10-12 19:45:40 -0300

    Criar midd responsável pelas rotas protegidas e pela expiração do token

---
+++ front-end/src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Rotas protegidas
+const protectedRoutes = ["/", "/create"];
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const token = request.cookies.get("session")?.value;
+
+  if (token) {
+    const payloadBase64 = token.split(".")[1]; // Obtem o payload do token
+    const payloadJson = atob(payloadBase64); // Decodifica a parte base64
+    const payload = JSON.parse(payloadJson); // Converte o payload para JSON
+
+    // Obtém o timestamp atual e converter para segundos
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    // Verificar se o token expirou, se sim, remove a sessão
+    if (payload.exp < currentTime - 60) {
+      const middResponse = NextResponse.next();
+
+      middResponse.cookies.delete("session");
+
+      return middResponse;
+    }
+  }
+
+  // Se for uma rota protegida, e estiver sem um token, redireciona para a rota de login
+  if (protectedRoutes.includes(pathname) && !token) {
+    return NextResponse.rewrite(new URL("/signin", request.url));
+  }
+
+  // Se não for um rota protegida, e tiver um token, redireciona para a rota principal
+  if (!protectedRoutes.includes(pathname) && token) {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
