677c931960ed6fac2f713bb55486b487fdbe0061
luiz2k <luizteles934@gmail.com>
2024-10-12 18:51:26 -0300

    Criar um modal reutilizavel

---
+++ front-end/src/modules/shared/components/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import s from "./Modal.module.css";
+import { X } from "lucide-react";
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  desc: string;
+  children: React.ReactNode;
+};
+
+// Modal reutilizado
+export function Modal({ isOpen, onClose, title, desc, children }: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className={s.container}>
+      <div className={s.modal}>
+        <div className={s.header}>
+          <h2>{title}</h2>
+          <p>{desc}</p>
+        </div>
+
+        {children}
+
+        <button type="button" className={s.close} onClick={onClose}>
+          <X />
+        </button>
+      </div>
+    </div>
+  );
+}
